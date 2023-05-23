0d3e5d1b7d043db5a3dc2c74c0822b658be82afb
NITHIN <nithingedda45@gmail.com>
2023-05-23 12:08:50 +0530

    Face-Recognition

--- FaceRecognition/imageRecognition/imgRecognition.py
+++ FaceRecognition/imageRecognition/imgRecognition.py
@@ -7,7 +7,7 @@ gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 face_rect = face_cascade.detectMultiScale(gray_img,1.1,5)
 for (x,y,w,h) in face_rect:
     cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-    cv2.imshow('DETECTED FACE',img)
+cv2.imshow('IMAGE',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 #print(round(cv2.rectangle))
